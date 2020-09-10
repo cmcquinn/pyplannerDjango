3639d59e9dd3f6d4c348a31a22004d825a15cdbe
Cameron McQuinn <cameron.mcquinn@gmail.com>
2020-09-09 18:28:05 -0600

    Create models

--- taskServe/models.py
+++ taskServe/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+CHAR_FIELD_MAX_LENGTH = 150
+
+
+class Calendar(models.Model):
+    name = models.CharField(max_length=CHAR_FIELD_MAX_LENGTH)
+    url = models.URLField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class Task(models.Model):
+    class TaskStatus(models.IntegerChoices):
+        TO_DO = 1
+        IN_PROGRESS = 2
+        DONE = 3
+
+    name = models.CharField(max_length=CHAR_FIELD_MAX_LENGTH)
+    description = models.TextField()
+    dueDate = models.DateTimeField()
+    workDate = models.DateField()
+    status = models.IntegerField(choices=TaskStatus)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+    calendar = models.ForeignKey(Calendar, on_delete=models.CASCADE)
