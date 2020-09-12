4825af74d99433b9c29f3ead18b66ebb61cfe43b
Cameron McQuinn <cameron.mcquinn@gmail.com>
2020-09-12 17:57:49 -0600

    Serialize object id

--- taskServe/serializers.py
+++ taskServe/serializers.py
@@ -6,10 +6,10 @@ from .models import Task, Calendar
 class TaskSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Task
-        fields = ['name', 'description', 'dueDate', 'workDate', 'status', 'owner', 'calendar']
+        fields = ['id', 'name', 'description', 'dueDate', 'workDate', 'status', 'owner', 'calendar']
 
 
 class CalendarSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Calendar
-        fields = ['name', 'url', 'owner']
+        fields = ['id', 'name', 'url', 'owner']
