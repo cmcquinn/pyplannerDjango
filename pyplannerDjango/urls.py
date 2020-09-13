2c27d6fe76038c64e154d2186d3abeb72e344cb5
Cameron McQuinn <cameron.mcquinn@gmail.com>
2020-09-12 18:56:49 -0600

    Split serializers and viewsets from top-level urls module

--- pyplannerDjango/urls.py
+++ pyplannerDjango/urls.py
@@ -16,23 +16,11 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth.models import User
-from rest_framework import routers, serializers, viewsets
+from rest_framework import routers
+
+
+from pyplannerDjango.views import UserViewSet
 from taskServe.urls import router as task_serve_router
-
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'is_staff']
-
-
-# ViewSets define the view behavior
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
 
 # routers provide an easy way of automatically determining the URL conf
 router = routers.DefaultRouter()
